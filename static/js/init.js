4a0425ea3702bb16eb9b8e8621f04e40e2a2da48
Soheil <skarimik@ucsd.edu>
2017-02-06 18:10:05 -0800

    just for update

--- static/js/init.js
+++ static/js/init.js
@@ -4,8 +4,30 @@ navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
 
 var cam_video_id = "camsource"
 
+function xabc(){
+     // Assign the <video> element to a variable
+    var video = document.getElementById(cam_video_id);
+    var options = {
+        "audio": false,
+        "video": true
+    };
+    // Replace the source of the video element with the stream from the camera
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia(options, function(stream) {
 
-window.addEventListener('DOMContentLoaded', function xabc() {
+            video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
+
+        }, function(error) {
+            console.log(error)
+        });
+        // Below is the latest syntax. Using the old syntax for the time being for backwards compatibility.
+        // navigator.getUserMedia({video: true}, successCallback, errorCallback);
+    } else {
+        $("#qr-value").text('Sorry, native web camera streaming (getUserMedia) is not supported by this browser...')
+        return;
+    }
+}
+window.addEventListener('DOMContentLoaded', function() {
     // Assign the <video> element to a variable
     var video = document.getElementById(cam_video_id);
     var options = {
@@ -36,6 +58,7 @@ window.addEventListener('DOMContentLoaded', function xabc() {
 })*/
 var cam = null;
 function startReading(){
+    xabc();
     
     cam = camera(cam_video_id);
     cam.start();
