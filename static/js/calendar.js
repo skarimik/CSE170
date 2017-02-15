2e10a877b4c5843f0c18f85c2505d7968aa7fc1f
Soheil <skarimik@ucsd.edu>
2017-02-14 23:16:36 -0800

    just for update

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -1,131 +1,32 @@
+var clientId = '353317309491-3ed3on54tpim02gc5sg7tt8njnfi24gs.apps.googleusercontent.com';
+var apiKey = 'AIzaSyAfF8MHYCh7ZVo189HR9MyQ6NOjK4J88Ck';
+var scopes = 'https://www.googleapis.com/auth/calendar';
 
-
-
-var event = {
-  'summary': 'Google I/O 2015',
-  'location': '800 Howard St., San Francisco, CA 94103',
-  'description': 'A chance to hear more about Google\'s developer products.',
-  'start': {
-    'dateTime': '2017-11-01T09:00:00-07:00',
-    'timeZone': 'America/Los_Angeles'
-  },
-  'end': {
-    'dateTime': '2017-11-01T17:00:00-07:00',
-    'timeZone': 'America/Los_Angeles'
-  },
-  'recurrence': [
-    'RRULE:FREQ=DAILY;COUNT=2'
-  ],
-  'attendees': [
-    {'email': 'lpage@example.com'},
-    {'email': 'sbrin@example.com'}
-  ],
-  'reminders': {
-    'useDefault': false,
-    'overrides': [
-      {'method': 'email', 'minutes': 24 * 60},
-      {'method': 'popup', 'minutes': 10}
-    ]
-  }
-};
-
-
-// Client ID and API key from the Developer Console
-var CLIENT_ID = '353317309491-vkamti6jshvv4l1ucp3i6jbs1rn7mthm.apps.googleusercontent.com';
-
-// Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
-
-var authorizeButton = document.getElementById('authorize-button');
-var signoutButton = document.getElementById('signout-button');
-
-/**
-*  On load, called to load the auth2 library and API client library.
-*/
 function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
+  gapi.client.setApiKey(apiKey);
+  window.setTimeout(checkAuth,1);
+  checkAuth();
 }
 
-/**
-*  Initializes the API client library and sets up sign-in state
-*  listeners.
-*/
-function initClient() {
-  gapi.client.init({
-    discoveryDocs: DISCOVERY_DOCS,
-    clientId: CLIENT_ID,
-    scope: SCOPES
-  }).then(function () {
-  // Listen for sign-in state changes.
-  gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-  // Handle the initial sign-in state.
-  updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-  });
+function checkAuth() {
+  gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true},
+      handleAuthResult);
 }
 
-/**
-*  Called when the signed in status changes, to update the UI
-*  appropriately. After a sign-in, the API is called.
-*/
-function updateSigninStatus(isSignedIn) {
-  if (isSignedIn) {
-    alert("you are signed in");
-    listUpcomingEvents();
+function handleAuthResult(authResult) {
+  var authorizeButton = document.getElementById('authorize-button');
+  if (authResult) {
+    authorizeButton.style.visibility = 'hidden';
+    makeApiCall();
   } else {
-    alert("you are not signed in");
-  }
+    authorizeButton.style.visibility = '';
+    authorizeButton.onclick = handleAuthClick;
+   }
 }
 
-/**
-*  Sign in the user upon button click.
-*/
 function handleAuthClick(event) {
-  gapi.auth2.getAuthInstance().signIn();
+  gapi.auth.authorize(
+      {client_id: clientId, scope: scopes, immediate: false},
+      handleAuthResult);
+  return false;
 }
-
-/**
-*  Sign out the user upon button click.
-*/
-function handleSignoutClick(event) {
-  gapi.auth2.getAuthInstance().signOut();
-}
-
-/**
-* Append a pre element to the body containing the given message
-* as its text node. Used to display the results of the API call.
-*
-* @param {string} message Text to be placed in pre element.
-*/
-function appendPre(message) {
-  var pre = document.getElementById('content');
-  var textContent = document.createTextNode(message + '\n');
-  pre.appendChild(textContent);
-}
-
-/**
-* Print the summary and start datetime/date of the next ten events in
-* the authorized user's calendar. If no events are found an
-* appropriate message is printed.
-*/
-var request;
-function listUpcomingEvents() {
-  request = gapi.client.calendar.events.insert({
-
-    'calendarId': 'primary',
-    'resource': event
-  });
-}
-function doIt(){
-request.execute(function(event) {
-  alert("we are trying to add event here");
-  appendPre('Event created: ' + event.htmlLink);
-});
-}
-
-handleClientLoad();
-
