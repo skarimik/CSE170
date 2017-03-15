e398ef96e7eda1f6b04688208b7b1a95717b0181
Soheil <skarimik@ucsd.edu>
2017-03-14 17:55:26 -0700

    switching camera

--- static/js/init.js
+++ static/js/init.js
@@ -27,17 +27,18 @@ function toggle(){
   if(x == 0){
     x = 1;
     val = $('select#videoSource option:eq(1)').val()
-   
+    alert($('select#videoSource option:eq(1)').val())
     
   }
   else if(x == 1){
     x = 0
-  
+   
     val = $('select#videoSource option:eq(0)').val()
-
+    alert($('select#videoSource option:eq(0)').val())
+    
   }
 
-  $('select#audioSource').val(val);
+  $('select#videoSource').val(val);
   starting();
 }
 function starting() {
