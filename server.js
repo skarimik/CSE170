6e466170d8f706d048068d4b1500de4d36a7502d
tonychan96 <tony_chan@intuit.com>
2020-05-23 18:17:44 -0700

    remved handlebars error

--- server.js
+++ server.js
@@ -9,12 +9,11 @@
 
 // Express is a web framework for node.js
 // that makes nontrivial applications easier to build
-var express = require('express');
-var handlebars = require('express3-handlebars');
-var http = require('http');
-var path = require('path');
-var event = require('./static/js/addEvents');
-
+var express = require("express");
+// var handlebars = require('express3-handlebars');
+var http = require("http");
+var path = require("path");
+var event = require("./static/js/addEvents");
 
 // Create the server instance
 var app = express();
@@ -23,33 +22,32 @@ var app = express();
 app.use(express.logger());
 app.use(express.compress());
 
-
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'static'));
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "static"));
+// app.engine('handlebars', handlebars());
+// app.set('view engine', 'handlebars');
 app.use(express.favicon());
-app.use(express.logger('dev'));
+app.use(express.logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.cookieParser('Intro HCI secret key'));
+app.use(express.cookieParser("Intro HCI secret key"));
 app.use(express.session());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, "static")));
 
 // Return all pages in the /static directory
 // whenever they are requested at '/'
 // e.g., http://localhost:3000/index.html
 // maps to /static/index.html on this machine
 // app.use(express.static(__dirname + '/static'));
-app.get('/event',event.eventsInfo);
+app.get("/event", event.eventsInfo);
 
 // Start the server
 //var port = process.env.PORT || PORT; // 80 for web, 3000 for development
 // app.listen(port, function() {
 // 	console.log("Node.js server running on port %s", port);
 // });
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get("port"), function () {
+  console.log("Express server listening on port " + app.get("port"));
 });
